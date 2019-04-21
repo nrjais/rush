d75167931bab62bff20f8a4142ac25d6e1da5baf
Neeraj Jaiswal <neerajj85@gmail.com>
2019-04-22 00:05:54 +0530

    Handle quotes in input

--- src/rush.rs
+++ src/rush.rs
@@ -7,7 +7,8 @@ pub enum Rush {
 
 impl Rush {
   pub fn from(line: String) -> Rush {
-    let mut iter = line.split_whitespace();
+    let tokens = tokenize(line);
+    let mut iter = tokens.iter();
     if let Some(c) = iter.next() {
       let args = iter.map(|s| s.to_string()).collect::<Vec<_>>();
       Bin(c.to_string(), args)
@@ -15,4 +16,30 @@ impl Rush {
       Empty
     }
   }
+}
+
+fn tokenize(input: String) -> Vec<String> {
+  let mut quote = false;
+  let mut escape = false;
+  let mut token = String::new();
+  let mut tokens = Vec::new();
+
+  for c in input.chars() {
+    match c {
+      '"' if !escape => {
+        quote = !quote;
+      }
+      ' ' if !escape && !quote => {
+        tokens.push(token);
+        token = String::new();
+      }
+      '\\' => escape = true,
+      _ => {
+        token.push(c);
+        escape = false;
+      }
+    }
+  }
+  tokens.push(token);
+  tokens
 }
