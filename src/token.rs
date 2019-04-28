84f1540cb05209779636fa5f14e638e78b748eb7
Neeraj Jaiswal <neerajj85@gmail.com>
2019-04-28 12:55:17 +0530

    Expand multiple concatenated var in command

--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,6 @@
 use crate::token::Token::{Empty, Value};
+use std::env;
+use std::env::VarError;
 
 #[derive(Clone)]
 pub enum Token {
@@ -38,7 +40,117 @@ impl Token {
 }
 
 fn replace_with_value(s: &String) -> String {
-  let x = s.matches("\\$[A-Z_]+");
-  println!("{:?}", x);
-  String::new()
+  parse_vars(s)
+      .iter_mut()
+      .map(|t| expand(t, |s| env::var(s)))
+      .collect()
+}
+
+fn expand(v: &mut String, resolver: fn(&str) -> Result<String, VarError>) -> String {
+  match v.chars().next() {
+    None => String::new(),
+    Some('$') => {
+      resolver(v.trim_start_matches('$')).unwrap_or_default()
+    }
+    Some('\\') => v.trim_start_matches('\\').to_owned(),
+    _ => v.to_owned()
+  }
+}
+
+fn parse_vars(s: &String) -> Vec<String> {
+  let mut matching = false;
+  let mut m = String::new();
+  let mut matches = Vec::new();
+  let mut escaped = false;
+
+  for c in s.chars() {
+    match c {
+      '$' if !escaped => {
+        matching = true;
+        matches.push(m);
+        m = String::from("$");
+      }
+      '$' => {
+        matches.push(m);
+        m = String::from("\\$")
+      }
+      '\\' => {
+        escaped = true;
+      }
+      'a'..='z' | 'A'..='Z' | '0'..='9' | '_' if matching => m.push(c),
+      _ if matching => {
+        matches.push(m);
+        m = String::new();
+        m.push(c);
+      }
+      _ => {
+        m.push(c);
+      }
+    }
+  }
+
+  matches.push(m);
+  matches
+}
+
+#[cfg(test)]
+pub mod tests {
+  use crate::token::{parse_vars, expand};
+
+  #[test]
+  fn should_parse_empty_string() {
+    let parsed = parse_vars(&String::from(""));
+    assert_eq!(vec![String::new()], parsed);
+  }
+
+  #[test]
+  fn should_parse_string_word() {
+    let parsed = parse_vars(&String::from("hello"));
+    assert_eq!(vec![String::from("hello")], parsed);
+  }
+
+  #[test]
+  fn should_parse_string_with_single_var() {
+    let parsed = parse_vars(&String::from("$hello"));
+    assert_eq!(vec![String::from(""), String::from("$hello")], parsed);
+  }
+
+  #[test]
+  fn should_parse_string_with_multiple_var() {
+    let parsed = parse_vars(&String::from("$hello$world"));
+    assert_eq!(vec![String::from(""), String::from("$hello"), String::from("$world")], parsed);
+  }
+
+  #[test]
+  fn should_parse_string_with_multiple_separated_var() {
+    let parsed = parse_vars(&String::from("$hello-$world"));
+    assert_eq!(vec![String::from(""), String::from("$hello"), String::from("-"), String::from("$world")], parsed);
+  }
+
+  #[test]
+  fn should_parse_with_escaped_var() {
+    let parsed = parse_vars(&String::from("$hello-\\$world"));
+    assert_eq!(vec![String::from(""), String::from("$hello"), String::from("-"), String::from("\\$world")], parsed);
+  }
+
+  #[test]
+  fn should_expand_var(){
+    let mut var = String::from("$H");
+    let actual = expand(&mut var, |_| Ok("Hello".to_owned()));
+    assert_eq!(String::from("Hello"), actual)
+  }
+
+  #[test]
+  fn should_not_expand_var(){
+    let mut var = String::from("H");
+    let actual = expand(&mut var, |_| Ok("Hello".to_owned()));
+    assert_eq!(String::from("H"), actual)
+  }
+
+  #[test]
+  fn should_not_expand_var_when_escaped(){
+    let mut var = String::from("\\$H");
+    let actual = expand(&mut var, |_| Ok("Hello".to_owned()));
+    assert_eq!(String::from("$H"), actual)
+  }
 }
